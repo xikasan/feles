d48f2eb9ec4b25a8672239cb191e99d9d4406f18
xikasan <xika@xika.jp>
2020-09-20 12:57:00 +0900

    dev test of utils.path

---
+++ tests/utils/test_path.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+import xtools as xt
+from feles.utils.path import go_to_root
+
+
+def run():
+    xt.info("test utils")
+    go_to_root()
+
+
+if __name__ == '__main__':
+    run()
